39e3ec1357c37cca7e797d8d8da977efdeabd456
Alina Skrypnychenko <skripnichenkoalya@gmail.com>
2025-04-14 20:29:44 +0200

    add scanner and read user input

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        sc.close();
+
+        System.out.println(userInput);
     }
 }
