dd267142b80e1a6c7c43178e7f7c15d3359e8352
Alina Skrypnychenko <skripnichenkoalya@gmail.com>
2025-04-15 11:22:58 +0200

    add .gitignore

--- src/Form.java
+++ src/Form.java
@@ -8,7 +8,7 @@ import javax.swing.SwingConstants;
 public class Form extends JFrame{
     public Form(){
         super("Password Generator");
-        setSize(500, 300);
+        setSize(540, 570);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);// center the window
         addComponents();
@@ -19,9 +19,14 @@ public class Form extends JFrame{
         JLabel nameLabel = new JLabel("Password Generator");
         nameLabel.setFont(new Font("Arial",Font.BOLD| Font.ITALIC,32));
         nameLabel.setForeground(new Color(0, 120, 215));
+        nameLabel. setBounds(0, 0, 540, 39);
         nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(nameLabel);
 
+       //JTextArea passOutput = new JTextArea();
+
+       
+        
 
 
 
