f535004207fbc42c2d034bf8644e74d7579d69d4
Alina Skrypnychenko <skripnichenkoalya@gmail.com>
2025-04-16 13:26:11 +0200

    add class PasswordGen and password field

--- src/Form.java
+++ src/Form.java
@@ -1,11 +1,21 @@
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class Form extends JFrame{
+    
+
+
     public Form(){
         super("Password Generator");
         setSize(540, 570);
@@ -16,14 +26,47 @@ public class Form extends JFrame{
     }
     
     private void addComponents(){
-        JLabel nameLabel = new JLabel("Password Generator");
-        nameLabel.setFont(new Font("Arial",Font.BOLD| Font.ITALIC,32));
-        nameLabel.setForeground(new Color(0, 120, 215));
-        nameLabel. setBounds(0, 0, 540, 39);
-        nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        add(nameLabel);
+        JLabel titleLabel = new JLabel("Password Generator");
+        titleLabel.setFont(new Font("Arial",Font.BOLD| Font.ITALIC,32));
+        titleLabel.setForeground(new Color(0, 120, 215));
+        titleLabel. setBounds(0, 0, 540, 39);
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        titleLabel.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
+        add(titleLabel, BorderLayout.NORTH);
 
-       //JTextArea passOutput = new JTextArea();
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+        JTextField passwordField = new JTextField();
+        passwordField.setEditable(false);
+        passwordField.setFont(new Font("Arial", Font.BOLD, 16));
+        passwordField.setHorizontalAlignment(JTextField.CENTER);
+        passwordField.setMaximumSize(new Dimension(Integer.MAX_VALUE,50));;
+        mainPanel.add(passwordField);
+        mainPanel.add(Box.createRigidArea(new Dimension(0,10))); 
+        passwordField.setBorder(BorderFactory.createLineBorder(Color.GRAY,1));
+        add(mainPanel);
+        
+
+
+       
+       /*  JTextArea passOutput = new JTextArea(); 
+       /*  passOutput.setEditable(false); // Text nicht verändert
+        //passOutput.setFont(new Font ("Dialog", Font.BOLD,32 ));
+        JScrollPane scrollPane = new JScrollPane(passOutput);
+        //scrollPane.setBounds(25,97 , 479, 78);
+        scrollPane.setBorder(BorderFactory.createLineBorder( Color.BLUE)); 
+        passOutput.setFont(new Font("Dialog", Font.BOLD, 24));
+        scrollPane.setBounds(25, 97, 479, 60); 
+        passOutput.setBackground(new Color(240, 240, 240));
+        passOutput.setAlignmentX(Component.BOTTOM_ALIGNMENT);
+        add(scrollPane);  */
+       
+
+    
+
+    
 
        
         
---
+++ src/PasswordGen.java
@@ -0,0 +1,18 @@
+
+import java.util.Random;
+
+public class PasswordGen {
+    public static final String NUMBERS ="0123456789";
+    public static final String SPECIAl_SYMBOLS ="!@#$%^&*()_+-=[]{}|;':\\\",./<>?";
+    public static final String LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+    public static final String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final Random random;
+
+    public PasswordGen(){
+        random = new Random();
+    }
+
+    
+
+    
+}
